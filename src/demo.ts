ada6eb901d6be468495dd000fe3bc8ab5b34c239
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-10 23:09:05 -0600

    interfaces

--- src/demo.ts
+++ src/demo.ts
@@ -1,9 +1,21 @@
-let x: number;
-let y: string;
-let z: boolean;
-let a: Date;
-let b: string[];
+interface Contact extends Address{
+    id: number;
+    name: string;
+    birthdate?: Date;
+}
 
-b = 'Hello' as any;
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
 
-
+let primaryContact: Contact = {
+    birthdate: new Date("o1-01-1980"),
+    id: 12345,
+    name: 'Jamie Johnson',
+    postalCode: '1234',
+    
+}
