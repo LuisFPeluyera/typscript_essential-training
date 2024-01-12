044fea9d70325cd19ae20cc98d22a3fdc04bee16
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-12 09:40:54 -0600

    type aliases

--- src/demo.ts
+++ src/demo.ts
@@ -1,21 +1,14 @@
-interface Contact extends Address{
+interface Contact {
     id: number;
-    name: string;
+    name: ContactName;
     birthdate?: Date;
-}
-
-interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
 }
 
 let primaryContact: Contact = {
     birthdate: new Date("o1-01-1980"),
     id: 12345,
     name: 'Jamie Johnson',
-    postalCode: '1234',
-    
 }
+
+type ContactName = string;
+
