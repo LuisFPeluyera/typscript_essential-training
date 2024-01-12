e7be7d9348beb35c6288a30bcc390033ddf4ea3a
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-12 11:10:57 -0600

    indexed access types

--- src/demo.ts
+++ src/demo.ts
@@ -1,44 +1,38 @@
-type ContactName = string;
-
-type ContactStatus = "active" | "inactive" | "new"
-
-
-type ContactBirthDate = Date | number | string;
-
-interface Contact {
-    id: number;
-    name: ContactName;
-    birthDate?: ContactBirthDate;
-    status?: ContactStatus;
-}
+type ContactStatus = "active" | "inactive" | "new";
 
 interface Address {
-    line1: string;
-    line2: string;
+    street: string;
     province: string;
-    region: string;
     postalCode: string;
 }
 
-type AddressableContact = Contact & Address
-
-function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
+interface Contact {
+    id: number;
+    name: string;
+    status: ContactStatus;
+    address: Address;
 }
 
-let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson",
-    status: "active"
+type Awesome = Contact["address"]["postalCode"]
+
+interface ContactEvent {
+    contactId: Contact["id"];
 }
 
-type ContactFileds = keyof Contact
+interface ContactDeletedEvent extends ContactEvent { 
+}
 
+interface ContactStatusChangedEvent extends ContactEvent { 
+    oldStatus: Contact["status"];
+    newStatus: Contact["status"];
+}
+
+interface ContactEvents {
+    deleted: ContactDeletedEvent;
+    statusChanged: ContactStatusChangedEvent;
+    // ... and so on
+}
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+    return source[propertyName];
+}
