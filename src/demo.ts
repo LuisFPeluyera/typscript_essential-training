c55790b9442bf9e227218ef13be1ecaa97f4dfd1
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-12 09:48:41 -0600

    enum types

--- src/demo.ts
+++ src/demo.ts
@@ -2,12 +2,20 @@ interface Contact {
     id: number;
     name: ContactName;
     birthdate?: Date;
+    status: ContactStatus;
+}
+
+enum ContactStatus {
+    Active = 'active', 
+    Inactive = 'inactive',
+    New = 'new'
 }
 
 let primaryContact: Contact = {
     birthdate: new Date("o1-01-1980"),
     id: 12345,
     name: 'Jamie Johnson',
+    status:ContactStatus.Active
 }
 
 type ContactName = string;
