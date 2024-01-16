5c756e74418f213262dbfa7abbddf6822b809696
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-15 22:40:38 -0600

    decorator parameters

--- src/demo.ts
+++ src/demo.ts
@@ -13,19 +13,19 @@ const currentUser = {
     }
 }
 
-function authorize(target: any, property: string, descriptor: PropertyDescriptor) {
-    const wrapped = descriptor.value
+function authorize(role: string) {
+    return function authorizeDecorator(target: any, property: string, descriptor: PropertyDescriptor) {
+        const wrapped = descriptor.value
 
-    descriptor.value = function () {
-        if (!currentUser.isAuthenticated()) {
-            throw Error("User is not authenticated");
-        }
+        descriptor.value = function () {
+            if (!currentUser.isAuthenticated()) {
+                throw Error("User is not authenticated");
+            }
+            if (!currentUser.isInRole(role)) {
+                throw Error(`User not in role ${role}`);
+            }
 
-        try {
             return wrapped.apply(this, arguments);
-        } catch (ex) {
-            // TODO: some fancy logging logic here
-            throw ex;
         }
     }
 }
@@ -35,10 +35,6 @@ class ContactRepository {
 
     @authorize("ContactViewer")
     getContactById(id: number): Contact | null {
-        if (!currentUser.isInRole("ContactViewer")) {
-            throw Error("User not authorized to execute this action");
-        }
-
         const contact = this.contacts.find(x => x.id === id);
         return contact;
     }
