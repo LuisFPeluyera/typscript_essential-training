4fb416df6ffe86d3d46688bcf685d8ac16a06236
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-15 22:30:56 -0600

    decorators

--- src/demo.ts
+++ src/demo.ts
@@ -1,45 +1,34 @@
-type ContactStatus = "active" | "inactive" | "new";
-
-interface Address {
-    street: string;
-    province: string;
-    postalCode: string;
-}
-
 interface Contact {
     id: number;
-    name: string;
-    status: ContactStatus;
-    address: Address;
-    email: string;
 }
 
-interface Query {
-    sort?: 'asc' | 'desc';
-    matches(val): boolean;
-}
-
-type ContactQuery = Partial<Record<keyof Contact, Query>>;
-
-function searchContacts(contacts: Contact[], query: ContactQuery) {
-    return contacts.filter(contact => {
-        for (const property of Object.keys(contact) as (keyof Contact)[]) {
-            // get the query object for this property
-            const propertyQuery = query[property];
-            // check to see if it matches
-            if (propertyQuery && propertyQuery.matches(contact[property])) {
-                return true;
-            }
-        }
-
-        return false;
-    })
-}
-
-const filteredContacts = searchContacts(
-    [/* contacts */],
-    {
-        id: { matches: (id) => id === 123 },
-        name: { matches: (name) => name === "Carol Weaver" },
+const currentUser = {
+    id: 1234,
+    roles: ["ContactEditor"],
+    isInRole(role: string): boolean {
+        return this.roles.contains(role);
     }
-);
+}
+
+@log
+class ContactRepository {
+    private contacts: Contact[] = [];
+
+    @authorize("ContactViewer")
+    getContactById(id: number): Contact | null {
+        const contact = this.contacts.find(x => x.id === id);
+
+        return contact;
+    }
+
+    @authorize("ContactEditor")
+    save(contact: Contact): void {
+        const existing = this.getContactById(contact.id);
+
+        if (existing) {
+            Object.assign(existing, contact);
+        } else {
+            this.contacts.push(contact);
+        }
+    }
+}
