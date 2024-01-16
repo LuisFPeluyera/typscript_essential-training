8811235d51340bce5003776db7904fe159e742a0
luisfpeluyera <luis.f.peluyera@gmail.com>
2024-01-15 22:06:05 -0600

    defining dynamic but limite types with records

--- src/demo.ts
+++ src/demo.ts
@@ -1,3 +1,12 @@
+let x:Record<string, string | number | boolean | Function> = { name: "Wruce Bayne" };
+x.number = 1234
+x.active = true;
+x.log = ()=> console.log('awesome')
+
+
+
+////////////////////
+
 type ContactStatus = "active" | "inactive" | "new";
 
 interface Address {
@@ -13,26 +22,31 @@ interface Contact {
     address: Address;
 }
 
-type Awesome = Contact["address"]["postalCode"]
-
-interface ContactEvent {
-    contactId: Contact["id"];
+interface Query {
+    sort?: 'asc' | 'desc';
+    matches(val): boolean;
 }
 
-interface ContactDeletedEvent extends ContactEvent { 
+function searchContacts(contacts: Contact[], query) {
+    return contacts.filter(contact => {
+        for (const property of Object.keys(contact)) {
+            // get the query object for this property
+            const propertyQuery = query[property];
+            // check to see if it matches
+            if (propertyQuery && propertyQuery.matches(contact[property])) {
+                return true;
+            }
+        }
+
+        return false;
+    })
 }
 
-interface ContactStatusChangedEvent extends ContactEvent { 
-    oldStatus: Contact["status"];
-    newStatus: Contact["status"];
-}
-
-interface ContactEvents {
-    deleted: ContactDeletedEvent;
-    statusChanged: ContactStatusChangedEvent;
-    // ... and so on
-}
-
-function getValue<T, U extends keyof T>(source: T, propertyName: U) {
-    return source[propertyName];
-}
+const filteredContacts = searchContacts(
+    [/* contacts */],
+    {
+        id: { matches: (id) => id === 123 },
+        name: { matches: (name) => name === "Carol Weaver" },
+        phoneNumber: { matches: (name) => name === "Carol Weaver" },
+    }
+);
